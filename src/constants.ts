1f7945a371f0a4c3cb95f0854ce22ad021938bbc
Victor Popkov <victor@popkov.me>
2022-01-19 09:01:59 +0300

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+export const constants = {
+  ERROR: {
+    ALREADY_RUNNING: 'Slack app is already running',
+    CHANNEL_NOT_FOUND: 'Slack channel not found',
+    INIT_FAILURE: 'Failed to initialize Slack app',
+    NOT_RUNNING: 'Slack app is not running',
+    START_FAILURE: 'Failed to start Slack app',
+    TOKEN_NOT_FOUND:
+      'Slack app token or signing secret not found. Did you forget to set SLACK_SIGNING_SECRET and/or SLACK_TOKEN environment variables?',
+    UNDEFINED_GITHUB_CONTEXT: 'GitHub %s context is undefined',
+  },
+} as const;
+
+export default constants;
