57d6f2277cbf984a9fc5ae3484323e9a977a19ce
Victor Popkov <victor@popkov.me>
2022-01-25 11:15:01 +0300

    Add Input

---
+++ src/input.ts
@@ -0,0 +1,71 @@
+import * as core from '@actions/core';
+
+export interface Input {
+  color: string;
+  fields: string[];
+  status: string;
+  text: string;
+  timestamp: string;
+}
+
+const getHEXColor = (name: string): string => {
+  const value = core.getInput(name);
+  if (value.length === 0) {
+    return '';
+  }
+  if (/^#[\dA-F]{6}$/i.test(value)) {
+    return value;
+  }
+  throw new Error(`Invalid ${name} input value. Should be a valid HEX color`);
+};
+
+function getStatus(name: string): string {
+  const value = core.getInput(name);
+  if (value.length === 0) {
+    return '';
+  }
+  if (
+    value === 'in-progress' ||
+    value === 'success' ||
+    value === 'failure' ||
+    value === 'cancelled' ||
+    value === 'skipped'
+  ) {
+    return value;
+  }
+  throw new Error(
+    `Invalid ${name} input value. Should be: in-progress|success|failure|cancelled|skipped`,
+  );
+}
+
+const getText = (name: string): string => {
+  const value = core.getInput(name);
+  if (value.length > 0) {
+    return value;
+  }
+  throw new Error(`Invalid ${name} input value. Shouldn't be an empty string`);
+};
+
+const getTimestamp = (name: string): string => {
+  const value = core.getInput(name);
+  if (value.length === 0 || new Date(parseFloat(value)).getTime() > 0) {
+    return value;
+  }
+  throw new Error(
+    `Invalid ${name} input value. Should be a valid UNIX timestamp`,
+  );
+};
+
+export async function get(): Promise<Input> {
+  try {
+    const input: Input = <Input>{};
+    input.color = getHEXColor('color');
+    input.fields = core.getMultilineInput('fields');
+    input.status = getStatus('status');
+    input.text = getText('text');
+    input.timestamp = getTimestamp('timestamp');
+    return input;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
