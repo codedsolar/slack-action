665c388ad5ebe65eec0982e00a6dbffbe68ab3a6
Victor Popkov <victor@popkov.me>
2022-02-11 20:18:49 +0300

    Remove unused INPUT_FAILURE constant

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,6 @@ export const constants = {
     ALREADY_RUNNING: 'Slack app is already running',
     CHANNEL_NOT_FOUND: 'Slack channel not found',
     INIT_FAILURE: 'Failed to initialize Slack app',
-    INPUT_FAILURE: 'Failed to initialize inputs',
     NOT_RUNNING: 'Slack app is not running',
     TOKEN_NOT_FOUND:
       'Slack app token or signing secret not found. Did you forget to set SLACK_SIGNING_SECRET and/or SLACK_TOKEN environment variables?',
