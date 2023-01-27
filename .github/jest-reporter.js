8a42400aa33076c3fae096e6e3861fd58444c6e3
Victor Popkov <victor@popkov.me>
2023-01-27 21:25:02 +0200

    Replace Jest reporter for ci GA workflow

--- .github/jest-reporter.js
+++
@@ -1,8 +0,0 @@
-class JestReporter {
-  onRunComplete(testContexts, results) {
-    console.log(`::set-output name=total::${results.numTotalTests || 0}`);
-    console.log(`::set-output name=passed::${results.numPassedTests || 0}`);
-  }
-}
-
-module.exports = JestReporter;
