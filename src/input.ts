b7c9b2bd8b407689f2f47d39014373cb2954f27d
Victor Popkov <victor@popkov.me>
2024-06-13 16:45:26 +0300

    Add Input TSDoc comments

--- src/input.ts
+++ src/input.ts
@@ -1,26 +1,73 @@
 import * as core from '@actions/core';
 import * as input from './toolkit/input';
 
+/**
+ * Class Input for retrieving and storing all input values.
+ */
 export default class Input {
+  /**
+   * Attachment color in HEX format.
+   */
   public color: string = '';
 
+  /**
+   * Fields in the multiline format.
+   * Each field must match: "<your name>: <your value>".
+   * Supported keywords: `{REF}`, `{STATUS}`.
+   */
   public fields: string[] = ['{STATUS}', '{REF}'];
 
+  /**
+   * When the action exits it will be with an exit code of 0.
+   */
   public ignoreFailures: boolean = false;
 
+  /**
+   * When the previous message to update isn't found, a new one will be posted
+   * instead.
+   */
   public ignoreMessageNotFound: boolean = true;
 
+  /**
+   * Port on which to run the Slack application.
+   */
   public port: number = 3000;
 
+  /**
+   * Port retries number for an automatic bumping of unavailable ports.
+   */
   public portRetries: number = 3;
 
+  /**
+   * Status: `unknown`, `in-progress`, `success`, `failure`, `cancelled`,
+   * `skipped`.
+   *
+   * Respects: `job.status`, `steps.<step id>.conclusion`,
+   * `steps.<step id>.outcome`.
+   *
+   * @see {@link https://docs.github.com/en/actions/learn-github-actions/contexts#job-context}
+   * @see {@link https://docs.github.com/en/actions/learn-github-actions/contexts#steps-context}
+   */
   public status: string = 'unknown';
 
+  /**
+   * Message text. Supported keywords: `{GITHUB_ACTOR}`, `{GITHUB_JOB}`,
+   * `{GITHUB_REF}`.
+   */
   public text: string =
     'GitHub Actions {GITHUB_JOB} job in {GITHUB_REF} by {GITHUB_ACTOR}';
 
+  /**
+   * Timestamp in Slack UNIX format of the previous message to update.
+   *
+   * Use-case: update an already posted message from the previous step based on
+   * the "slack-timestamp" output.
+   */
   public timestamp: string = '';
 
+  /**
+   * Get all inputs.
+   */
   public async get(): Promise<Input> {
     try {
       this.color = input.getHEXColor('color');
