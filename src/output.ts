22dce127af62db7bfe92024819831d9c61444fa5
Victor Popkov <victor@popkov.me>
2024-06-13 16:45:58 +0300

    Add Output TSDoc comments

--- src/output.ts
+++ src/output.ts
@@ -1,8 +1,20 @@
 import * as core from '@actions/core';
 
+/**
+ * Class Output for retrieving and storing all output values.
+ */
 export default class Output {
+  /**
+   * Previous Slack message timestamp
+   *
+   * Use-case: use to update an already posted message by using this value as
+   * the "timestamp" input value.
+   */
   public slackTimestamp: string = '';
 
+  /**
+   * Set all outputs.
+   */
   public async set(): Promise<Output> {
     try {
       core.startGroup('Set output');
